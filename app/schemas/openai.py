acca94b8fde5f79b09f17311093fe51e0c0f7039
奥村 英璃奈 Ｈテクノ Ｄマネ ＳｏＲ部 <erina.okumura@hakuhodody-one.co.jp>
2025-08-05 15:53:14 +0900

    コミット漏れ追加

--- app/schemas/openai.py
+++ app/schemas/openai.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
  
 class GenerateTweetResponse(BaseModel):
-    tweet_text: str
-    commit_message: str
-    repository: str 
+    tweet_draft: str
+    commit_message: str 
